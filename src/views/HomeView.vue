a0a7eee65b21db904a25054fed1035689d301830
brcls <petrick_barcelos@hotmail.com>
2024-04-28 15:27:37 -0300

    wip: trabalhando home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,44 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>oi</div>
+  <div>
+    div
+
+    <div class="container full-height">
+      <img class="image" src="../assets/alan-turing.jpg" alt="Alan Turing" />
+      <h1 class="title">
+        “I believe that sometimes the people who don't expect anything are the
+        ones who achieve the most.”
+      </h1>
+    </div>
+    <div class="container full-height"></div>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 10rem;
+  padding: 0 10rem 0 0;
+}
+
+.full-height {
+  height: 100svh;
+}
+
+.image {
+  height: 100%;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
